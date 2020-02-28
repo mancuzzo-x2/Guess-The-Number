3bafdb1f42155781bf6427e98e3bfe9b53bd9ef4
mancuzzo-x2 <61546428+mancuzzo-x2@users.noreply.github.com>
2020-02-28 23:05:07 +0100

    GTN fix

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -2,21 +2,34 @@ import random
 
 cicle = True
 
+random_number = random.randrange(0, 20)
+
 while cicle == True:
 
-        random_number = random.randrange(0, 20)
-
-        guess = int(input("What's your guess?"))
+        guess = int(input("\nGuess a number between 0 and 20: "))
 
         if guess == random_number:
 
-                print("You hit the right answer!")
+                print("\nYou hit the right answer!\n")
+
+                ans = input("\nDo you want to retry?: Press Y for another try or press any other key to quit: ")
+
+                if ans == 'Y' or ans == 'y':
+
+                        cicle = True
+
+                        random_number = random.randrange(0, 20)
+
+                else:
+
+                        cicle = False
+                
 
         elif guess < random_number:
 
-                ans = input("Your guess was too low, do you want to retry?: Press Y for another try or press any other key to quit.")
+                ans = input("\nYour guess was too low, do you want to retry?: Press Y for another try or press any other key to quit: ")
 
-                if ans == Y or ans == y:
+                if ans == 'Y' or ans == 'y':
 
                         cicle = True
 
@@ -26,9 +39,9 @@ while cicle == True:
 
         elif guess > random_number:
 
-                ans = input("Your guess was too high, do you want to retry?: Press Y if you want to make another guess, or press any other key to quit.")
+                ans = input("\nYour guess was too high, do you want to retry?: Press Y if you want to make another guess, or press any other key to quit: ")
 
-                if ans == Y or ans == y:
+                if ans == 'Y' or ans == 'y':
 
                         cicle = True
 
