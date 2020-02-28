ed2047447a509f598389aecf67067b10744b6869
mancuzzo-x2 <61546428+mancuzzo-x2@users.noreply.github.com>
2020-02-28 22:34:15 +0100

    GTN first version

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,40 +1,37 @@
 import random
 
-cicle = true
+cicle = True
 
-while cicle == true:
+while cicle == True:
 
-	random_number = random.randrange(0, 20)
+        random_number = random.randrange(0, 20)
 
-	guess = int(input("What's your guess?"))
+        guess = int(input("What's your guess?"))
 
-	if guess == random_number:
+        if guess == random_number:
 
-		print("You hit the right answer!")
+                print("You hit the right answer!")
 
-	else if guess < random_number:
+        elif guess < random_number:
 
-		ans = input("Your guess was too low, do you want to retry?: Press Y for another try or press any other key to quit.")
-
-		if ans == Y or ans == y:
-
-			cicle = true
-
-		else:
-
-			cicle = false
-
-	else:
-
-                ans = input("Your guess was too high, do you want to retry?: Pre>
+                ans = input("Your guess was too low, do you want to retry?: Press Y for another try or press any other key to quit.")
 
                 if ans == Y or ans == y:
 
-                        cicle = true
+                        cicle = True
 
                 else:
 
-                        cicle = false
+                        cicle = False
 
+        elif guess > random_number:
 
-return
+                ans = input("Your guess was too high, do you want to retry?: Press Y if you want to make another guess, or press any other key to quit.")
+
+                if ans == Y or ans == y:
+
+                        cicle = True
+
+                else:
+
+                        cicle = False
