08aefe3d6b2e4ecee8d8c01839e3e339b0b28cac
mancuzzo-x2 <salehbunduka.kumara@gmail.com>
2020-02-29 16:55:45 +0100

    First version of "Guess the number" implemented using class.

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,50 +1,102 @@
 import random
 
-cicle = True
+class GTNum:
 
-random_number = random.randrange(0, 20)
+	def __init__(self, range = 20, guess = 0):
 
-while cicle == True:
+		self.range = range
+		self.guess = guess
 
-        guess = int(input("\nGuess a number between 0 and 20: "))
+	def Set_range(self):
 
-        if guess == random_number:
+		self.range = input("Insert an integer >= 0: ")
 
-                print("\nYou hit the right answer!\n")
+	def Set_guess(self):
 
-                ans = input("\nDo you want to retry?: Press Y for another try or press any other key to quit: ")
+		self.guess = input("\nGuess a number between 0 and " + str(self.range) + ": ")
 
-                if ans == 'Y' or ans == 'y':
+	def Get_guess(self):
 
-                        cicle = True
+		return self.guess
 
-                        random_number = random.randrange(0, 20)
+	def Get_range(self):
 
-                else:
+		return self.range
 
-                        cicle = False
-                
+	def GTC(self):
 
-        elif guess < random_number:
+		rn = random.randrange(0, self.range)
 
-                ans = input("\nYour guess was too low, do you want to retry?: Press Y for another try or press any other key to quit: ")
+		if self.guess == rn:
 
-                if ans == 'Y' or ans == 'y':
+			return 0
 
-                        cicle = True
+		elif self.guess < rn:
 
-                else:
+			return -1
 
-                        cicle = False
+		else:
 
-        elif guess > random_number:
+			return 1
 
-                ans = input("\nYour guess was too high, do you want to retry?: Press Y if you want to make another guess, or press any other key to quit: ")
+	def GTN(self):
 
-                if ans == 'Y' or ans == 'y':
+		print("Define a range from 0 to N.")
 
-                        cicle = True
+		self.Set_range()
+		self.Set_guess()
 
-                else:
+		range = self.Get_range()
+		guess = self.Get_guess()
 
-                        cicle = False
+		while True:
+
+			compare = self.GTC()
+
+	        	if compare == 0:
+
+        	        	print("\nYou hit the right answer!\n")
+				ans = raw_input("\nDo you want to retry?\nPress Y for yes or N for no: ")
+
+				if ans == "Y" or ans == "y":
+
+					guess = self.Set_guess()
+					continue
+
+				else:
+
+					break
+
+	        	elif compare == -1:
+
+	                	print("\nYour guess was too low...")
+				ans = raw_input("\nDo you want to retry?\nPress Y for yes or N for no: ")
+
+                                if ans == "Y" or ans == "y":
+
+                                        guess = self.Set_guess()
+                                        continue 
+
+                                else:
+
+                                        break
+
+        		else:
+
+                		print("\nYour guess was too high...")
+				ans = raw_input("\nDo you want to retry?\nPress Y for yes or N for no: ")
+
+                                if ans == "Y" or  ans == "y":
+
+                                        guess = self.Set_guess()
+                                        continue 
+
+                                else:
+
+                                        break
+
+		return;
+
+
+x = GTNum()
+x.GTN()
